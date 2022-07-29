77fcccfe20ad07c9135912e86e050256fdef7aa0
Abduganiyev <mr.show050@gmail.com>
2022-07-29 23:31:27 +0500

    first commit

---
+++ src/main/java/com/webbrain/wherepizza/WherePizzaApplication.java
@@ -0,0 +1,13 @@
+package com.webbrain.wherepizza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WherePizzaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WherePizzaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/webbrain/wherepizza/WherePizzaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webbrain.wherepizza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WherePizzaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
