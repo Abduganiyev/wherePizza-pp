759d7fb2568138c64d69dc4420c7d1a20fcba36e
Abduganiyev <mr.show050@gmail.com>
2022-07-31 23:23:35 +0500

    add-AttachmentController

---
+++ src/main/java/com/webbrain/wherepizza/controller/AttachmentController.java
@@ -0,0 +1,11 @@
+package com.webbrain.wherepizza.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/attachment")
+@RequiredArgsConstructor
+public class AttachmentController {
+}
